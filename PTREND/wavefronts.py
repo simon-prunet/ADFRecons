3b8664ae0ddca1c19083434853f50595a45491f2
luckyjim <jcolley@lpnhe.in2p3.fr>
2022-12-08 13:45:00 +0100

    add numba cache, add xxx_ref() for reference function xxx() after merge

--- PTREND/wavefronts.py
+++ PTREND/wavefronts.py
@@ -16,7 +16,7 @@ B_inc = np.pi/2. + 1.0609856522873529
 Bvec = np.array([np.sin(B_inc)*np.cos(B_dec),np.sin(B_inc)*np.sin(B_inc),np.cos(B_inc)])
 
 
-kwd = {"fastmath": {"reassoc", "contract", "arcp"}}
+kwd = {"cache": True, "fastmath": {"reassoc", "contract", "arcp"}}
 
 
 
@@ -36,6 +36,64 @@ def RefractionIndexAtPosition(X):
     rh = ns*np.exp(kr*h)
     n = 1.+1e-6*rh
     return (n)
+
+@njit
+def ZHSEffectiveRefractionIndex_ref(X0,Xa):
+
+    R02 = X0[0]**2 + X0[1]**2
+    
+    # Altitude of emission in km
+    h0 = (np.sqrt( (X0[2]+R_earth)**2 + R02 ) - R_earth)/1e3
+    # print(h0)
+    
+    # Refractivity at emission 
+    rh0 = ns*np.exp(kr*h0)
+
+    modr = np.sqrt(R02)
+    # print(modr)
+
+    if (modr > 1e3):
+
+        # Vector between antenna and emission point
+        U = Xa-X0
+        # Divide into pieces shorter than 10km
+        nint = np.int(modr/2e4)+1
+        K = U/nint
+
+        # Current point coordinates and altitude
+        Curr  = X0
+        currh = h0
+        s = 0.
+
+        for i in np.arange(nint):
+            Next = Curr + K # Next point
+            nextR2 = Next[0]*Next[0] + Next[1]*Next[1]
+            nexth  = (np.sqrt( (Next[2]+R_earth)**2 + nextR2 ) - R_earth)/1e3
+            if (np.abs(nexth-currh) > 1e-10):
+                s += (np.exp(kr*nexth)-np.exp(kr*currh))/(kr*(nexth-currh))
+            else:
+                s += np.exp(kr*currh)
+
+            Curr = Next
+            currh = nexth
+            # print (currh)
+
+        avn = ns*s/nint
+        # print(avn)
+        n_eff = 1. + 1e-6*avn # Effective (average) index
+
+    else:
+
+        # without numerical integration
+        hd = Xa[2]/1e3 # Antenna altitude
+        #if (np.abs(hd-h0) > 1e-10):
+        avn = (ns/(kr*(hd-h0)))*(np.exp(kr*hd)-np.exp(kr*h0))
+        #else:
+        #    avn = ns*np.exp(kr*h0)
+
+        n_eff = 1. + 1e-6*avn # Effective (average) index
+
+    return (n_eff)
 
 
 @njit(cache=True, fastmath=True)
@@ -294,8 +352,9 @@ def PWF_loss(params, Xants, tants, cr=1.0, verbose=False):
     return (chi2)
 ###################################################
 
-@njit(cache=True, fastmath=True)
-def SWF_loss_nok(params, Xants, tants, cr=1.0, verbose=False):
+
+@njit
+def SWF_loss_ref(params, Xants, tants, cr=1.0, verbose=False):
 
     '''
     Defines Chi2 by summing model residuals over antennas  (i):
@@ -306,7 +365,6 @@ def SWF_loss_nok(params, Xants, tants, cr=1.0, verbose=False):
     x_s = \sin(\theta)\cos(\phi)
     y_s = \sin(\theta)\sin(\phi)
     z_s = \cos(\theta)
-
     Inputs: params = theta, phi, r_xmax, t_s
     \theta, \phi are the spherical coordinates of the vector K
     t_s is the source emission time
@@ -325,23 +383,19 @@ def SWF_loss_nok(params, Xants, tants, cr=1.0, verbose=False):
         print("Shapes of tants and Xants are incompatible",tants.shape, Xants.shape)
         return None
     tmp = 0.
-    dX = Xants - Xmax
-    #print(Xants.shape, Xmax.shape)
-    delta_t = tants - t_s
-    #print(tants.shape, t_s.shape)
     for i in range(nants):
         # Compute average refraction index between emission and observer
-        n_average = ZHSEffectiveRefractionIndex(Xmax, Xants[i,:])
+        n_average = ZHSEffectiveRefractionIndex_ref(Xmax, Xants[i,:])
         ## n_average = 1.0 #DEBUG
+        dX = Xants[i,:] - Xmax
         # Spherical wave front
-        res = cr*delta_t[i] - n_average*np.linalg.norm(dX[i])
+        res = cr*(tants[i]-t_s) - n_average*np.linalg.norm(dX)
         tmp += res*res
 
     chi2 = tmp
     if (verbose):
         print ("Chi2 = ",chi2)
     return(chi2)
-
 
 #@njit(cache=True, fastmath=True)
 @njit(**kwd)
@@ -390,9 +444,8 @@ def SWF_loss(params, Xants, tants, cr=1.0, verbose=False):
     return(chi2)
 
 
-
-@njit(cache=True, fastmath=True)
-def SWF_grad_nok(params, Xants, tants, cr=1.0, verbose=False):
+@njit
+def SWF_grad_ref(params, Xants, tants, cr=1.0, verbose=False):
     '''
     Gradient of SWF_loss, w.r.t. theta, phi, r_xmax and t_s
     '''
@@ -411,45 +464,7 @@ def SWF_grad_nok(params, Xants, tants, cr=1.0, verbose=False):
     
     jac = np.zeros(4)
     for i in range(nants):
-        n_average = ZHSEffectiveRefractionIndex(Xmax, Xants[i,:])
-        ## n_average = 1.0 ## DEBUG
-        dX = Xants[i,:] - Xmax
-        ndX = np.linalg.norm(dX)
-        res = cr*(tants[i]-t_s) - n_average*ndX
-        one_dX = (2*n_average* res/ndX)*dX
-        # Derivatives w.r.t. theta, phi, r_xmax, t_s
-        jac[0] += -np.dot(-dXmax_dtheta,one_dX)
-        jac[1] += -np.dot(-dXmax_dphi,  one_dX)
-        jac[2] += -np.dot(-dXmax_drxmax,one_dX)
-        jac[3] += -2*cr                                     * res 
-    # if (verbose):
-    #     print ("Jacobian = ",jac)
-    return(jac)
-
-
-
-#@njit(cache=True, fastmath=True)
-@njit(**kwd)
-def SWF_grad_ori(params, Xants, tants, cr=1.0, verbose=False):
-    '''
-    Gradient of SWF_loss, w.r.t. theta, phi, r_xmax and t_s
-    '''
-    theta, phi, r_xmax, t_s = params
-    # print("theta,phi,r_xmax,t_s = ",theta,phi,r_xmax,t_s)
-    nants = tants.shape[0]
-    ct = np.cos(theta); st = np.sin(theta); cp = np.cos(phi); sp = np.sin(phi)
-    K = np.array([st*cp,st*sp,ct])
-    Xmax = -r_xmax * K + np.array([0.,0.,groundAltitude]) # Xmax is in the opposite direction to shower propagation.
-    # Derivatives of Xmax, w.r.t. theta, phi, r_xmax
-    dK_dtheta = np.array([ct*cp,ct*sp,-st])
-    dK_dphi   = np.array([-st*sp,st*cp,0.])
-    dXmax_dtheta = -r_xmax*dK_dtheta
-    dXmax_dphi   = -r_xmax*dK_dphi
-    dXmax_drxmax = -K
-    
-    jac = np.zeros(4)
-    for i in range(nants):
-        n_average = ZHSEffectiveRefractionIndex(Xmax, Xants[i,:])
+        n_average = ZHSEffectiveRefractionIndex_ref(Xmax, Xants[i,:])
         ## n_average = 1.0 ## DEBUG
         dX = Xants[i,:] - Xmax
         #ndX = np.linalg.norm(dX)
@@ -463,6 +478,8 @@ def SWF_grad_ori(params, Xants, tants, cr=1.0, verbose=False):
     # if (verbose):
     #     print ("Jacobian = ",jac)
     return(jac)
+
+
 #@njit(cache=True, fastmath=True)
 @njit(**kwd)
 def SWF_grad(params, Xants, tants, cr=1.0, verbose=False):
