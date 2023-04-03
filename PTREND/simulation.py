7995f01450e3d88f66eb854e5b90c1d528404aec
Simon Prunet <prunet@cfht.hawaii.edu>
2023-04-03 14:20:02 +0200

    Finished first version of simulation class. Usage:
    from simulation import simulations
    s = Simulations('../Chiche/coord_antennas.txt')
    params = np.array(np.deg2rad([112.,270.]))
    s(params,sigma_t = 5e-9)
    
    Writes down Rec_coinctable_simulated.txt, that can be used, together with coord_antennas.txt,
    to launch a reconstruction with recons.py.
    By default, the model is assumed to be a plane wave, with a wave vector of angular coordinates (theta,phi) in rad.
    
    For the spherical wave case, we need to add to the parameters r_xmax and t_s (both in m), distance from (0,0,groundAltitude) point to Xmax, and
    time of emission respectively.
    
    params = np.hstack( (np.array(np.deg2rad([112.,270.]))), np.array([3e4,-3e4]) )
    s(params,simulation_type='SWF',sigma_t=5e-9)

--- PTREND/simulation.py
+++ PTREND/simulation.py
@@ -13,35 +13,40 @@ class Simulations:
 	def __init__(self,antenna_position_file,coincidence_output_file='Rec_coinctable_simulated.txt'):
 
 		self.antennas = antenna_set(antenna_position_file)
-		self.simulation_type = 'PWF' # 
+		self.coincidence_output_file=coincidence_output_file
 	
-	def write_coincidence_file(self,coincidence_output_file):
+	def write_coincidence_file(self,tants,coincidence_output_file):
 
 		# Set seed to produce the same (random) integer event ids
-		numpy.random.seed(1)
+		np.random.seed(1)
 
+		f = open(coincidence_output_file,'w')
 		for i in range(self.number_of_events):
 			event_id = np.random.random_integers(max_id) # Create a random number < max_id
 			for j in range(self.antennas.nants):
-				# To be continued...
+				f.write("{0:<4d} {1:<10d} {2:<12.10g} {3:<12.10g}\n".format(j,event_id,tants[i,j]/c_light,np.nan))
+		f.close()
+		return
 
-	def __call__(self,params,simulation_type='PWF',iseed=1234, number_of_events=1):
+	def __call__(self,params,simulation_type='PWF',iseed=1234, sigma_t = 5e-9, number_of_events=1):
 
 		self.number_of_events = number_of_events
+		self.simulation_type = simulation_type
+
 		tants = np.zeros((number_of_events,self.antennas.nants))
 		if (self.simulation_type=='PWF'):
-			tants[0,:] = PWF_simulation(params, self.antennas.coordinates, iseed=iseed)
+			tants[0,:] = PWF_simulation(params, self.antennas.coordinates, iseed=iseed, sigma_t=sigma_t)
 			for i in range(1, number_of_events):
-				tants[i,:] = PWF_simulation(params, self.antennas.coordinates)
+				tants[i,:] = PWF_simulation(params, self.antennas.coordinates, sigma_t=sigma_t)
 		elif (self.simulation_type=='SWF'):
-			tants[0,:] = SWF_simulation(params, self.antennas.coordinates, iseed=iseed)
+			tants[0,:] = SWF_simulation(params, self.antennas.coordinates, iseed=iseed, sigma_t=sigma_t)
 			for i in range(1, number_of_events):
-				tants[i,:] = SWF_simulation(params, self.antennas.coordinates)
+				tants[i,:] = SWF_simulation(params, self.antennas.coordinates,sigma_t=sigma_t)
 		else:
 			print('Only PWF (plane wave) and SWF (spherical wave) are supported now')
 			return
 
-		self.write_coincidence_file(self,tants,coincidence_output_file)
+		self.write_coincidence_file(tants,self.coincidence_output_file)
 		return
 
 
